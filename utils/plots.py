e83d69409392c0a4ed3de0213810217676a8b525
Szymon Domzalski <100307341+szdomzalski@users.noreply.github.com>
2024-09-20 20:55:48 +0200

    Added comments to baffling lines

--- utils/plots.py
+++ utils/plots.py
@@ -17,7 +17,9 @@ def plot_decision_regions(X: NDArray, y: NDArray, classifier: any, resolution: f
     x2_max = X[:, 1].max() + 1
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    # xx1 and xx2 are matrices and need to be flattened to 2 columns of X matrix
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    # Reshaping to mesh dimensions
     Z = Z.reshape(xx1.shape)
 
     plt.figure()
@@ -27,4 +29,4 @@ def plot_decision_regions(X: NDArray, y: NDArray, classifier: any, resolution: f
 
     for i, class_label in enumerate(np.unique(y)):
         plt.scatter(X[y == class_label, 0], X[y == class_label, 1], alpha=0.8, c=colors[i], marker=markers[i],
-                    label=class_label, edgecolor='black')
+                    label=class_label, edgecolors='black')
