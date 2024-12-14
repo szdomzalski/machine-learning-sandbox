5b3b0aac7f2e88837d823e8d92551ab765e8190e
Szymon Domzalski <100307341+szdomzalski@users.noreply.github.com>
2024-12-14 20:42:55 +0100

    Added example of logistic regression with scikit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from ml_lib.scikit import iris
-from ml_lib import iris_logistic_regression
+# from ml_lib import iris_logistic_regression
+from ml_lib.scikit.iris import linear_model_lr
 
-
-iris_logistic_regression.main()
+linear_model_lr.main()
---
+++ ml_lib/scikit/iris/linear_model_lr.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model, metrics, model_selection, preprocessing
+
+from ml_lib.utils.plots import plot_decision_regions
+
+
+def main():
+    iris = datasets.load_iris()
+
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    print('Class labels:', np.unique(y))
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+    # Stratification check
+    print(f'Number of each label occurences in dataset y: {np.bincount(y)}')
+    print(f'Number of each label occurences in training subset: {np.bincount(y_train)}')
+    print(f'Number of each label occurences in testing subset: {np.bincount(y_test)}')
+
+    # Scaling features (zeroing mean and setting unital variance)
+    std_scaler = preprocessing.StandardScaler()
+    std_scaler.fit(X_train)  # we assume train data only to simulate real life environment
+    X_train_std = std_scaler.transform(X_train)
+    X_test_std = std_scaler.transform(X_test)
+
+    model = linear_model.LogisticRegression(C=1000.0, random_state=1)
+    model.fit(X_train_std, y_train)
+
+    y_predict = model.predict(X_test_std)
+
+    print(f'Number of falsely classified test samples: {(y_test != y_predict).sum()} / {len(y_test)}')
+    print(f'Ratio of falsely classified test samples: {(y_test != y_predict).sum()/len(y_test):.2f}')
+    print(f'Accuracy score: {metrics.accuracy_score(y_test, y_predict):.2f}')
+
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+
+    plot_decision_regions(X_combined_std, y_combined, model, test_idx=[*range(105, 150)])
+
+    plt.ylabel('Petal width [standardized]')
+    plt.xlabel('Petal length [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
