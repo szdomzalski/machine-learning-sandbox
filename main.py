83d5c0281a90f10200052b07001e79bb207adb54
Szymon Domzalski <100307341+szdomzalski@users.noreply.github.com>
2024-12-27 20:46:50 +0100

    Added SVM with scikit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from ml_lib.scikit import iris
 # from ml_lib import iris_logistic_regression
-from ml_lib.scikit.iris import linear_model_lr
+# from ml_lib.scikit.iris import linear_model_lr
+from ml_lib.scikit.iris import svm
 
-linear_model_lr.main()
+svm.main()
