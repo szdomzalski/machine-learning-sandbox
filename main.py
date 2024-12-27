a0bcd3068fd004be97c17b71c09042590c968f0e
Szymon Domzalski <100307341+szdomzalski@users.noreply.github.com>
2024-12-27 21:10:17 +0100

    Added non-linear XOR to kernelize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from ml_lib.scikit import iris
 # from ml_lib import iris_logistic_regression
 # from ml_lib.scikit.iris import linear_model_lr
-from ml_lib.scikit.iris import svm
+# from ml_lib.scikit.iris import svm
+from ml_lib.scikit.iris import svm_kernel
 
-svm.main()
+svm_kernel.main()
